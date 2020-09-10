3c1c4eba58cad9ff40f4b226b0b99484d126352e
zengj4 <zengj4@uw.edu>
2020-09-10 16:45:53 -0700

    test

--- app/src/main/java/com/example/covidriskanalysisapp/ui/login/LoggedInUserView.java
+++ app/src/main/java/com/example/covidriskanalysisapp/ui/login/LoggedInUserView.java
@@ -3,10 +3,12 @@ package com.example.covidriskanalysisapp.ui.login;
 /**
  * Class exposing authenticated user details to the UI.
  */
+
+//test 
 class LoggedInUserView {
     private String displayName;
     //... other data fields that may be accessible to the UI
-
+    //branch test
     LoggedInUserView(String displayName) {
         this.displayName = displayName;
     }
